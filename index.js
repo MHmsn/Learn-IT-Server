20ddcb1359760934f60ded8b2a83658a36214f32
MHmsn <moinulh.msn@gmail.com>
2022-10-25 12:29:45 +0600

    added category related courses

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json")
 
 app.get("/", (req, res) => {
     res.send("API running");
@@ -10,9 +11,14 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
     res.send(categories);
-})
+});
 
+app.get("/categories/:id", (req, res) => {
+    const id = req.params.id;
+    const category_courses = courses.filter((i) => i.category_id === id);
+    res.send(category_courses);
+});
 
 app.listen(port, () => {
     console.log("server running on port", port);
-})
+});
