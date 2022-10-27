93369feac01682edbdfcf1493b53367cf4922b28
MHmsn <moinulh.msn@gmail.com>
2022-10-27 20:55:47 +0600

    added more data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+var cors = require('cors');
 const port = process.env.port || 5000;
+
+app.use(cors());
 
 const categories = require("./data/categories.json");
 const courses = require("./data/courses.json")
@@ -13,10 +16,21 @@ app.get("/categories", (req, res) => {
     res.send(categories);
 });
 
+app.get("/categories/course-:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find((n) => n.id === id);
+    res.send(selectedCourse);
+  });
+
 app.get("/categories/:id", (req, res) => {
     const id = req.params.id;
-    const category_courses = courses.filter((i) => i.category_id === id);
-    res.send(category_courses);
+    if(id === '07'){
+        res.send(courses);
+    }
+    else{
+       const category_courses = courses.filter((i) => i.category_id === id);
+    res.send(category_courses);  
+    }
 });
 
 app.listen(port, () => {
